cbf31fb00b1c08a6d5dfdd34522535395b4ccfbb
Shu Dong <shudong@fb.com>
2019-02-10 01:29:09 -0800

    whitepaper poster creation

--- apps/whitepaper_journal/run.py
+++ apps/whitepaper_journal/run.py
@@ -3,47 +3,85 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import copy
 from toolset.image.composer import ImageComposer, ImagePiece
 from toolset.google.sheet import GoogleSheet
 import toolset.utils as utils
-
 from app import App
 
 
 class WhitepaperJournalPoster:
-    def __init__(self, tempalte_path):
-        self.header = ImagePiece(os.path.join(tempalte_path, 'header.png'))
-        self.schedule = ImagePiece(os.path.join(tempalte_path, 'schedule.png'))
-        self.topic = ImagePiece(os.path.join(tempalte_path, 'topic.png'))
-        self.item = ImagePiece(os.path.join(tempalte_path, 'item.png'))
-        self.item_sep = ImagePiece(os.path.join(tempalte_path, 'item_sep.png'))
-        self.logo = ImagePiece(os.path.join(tempalte_path, 'logo.png'))
+    def __init__(self, settings):
+        self.settings = settings
+        self.init_imgs()
+
+    def init_imgs(self):
+        template_path = self.settings['template_path']
+        self.header = ImagePiece(os.path.join(template_path, 'header.png'))
+        self.tail = ImagePiece(os.path.join(template_path, 'tail.png'))
+        self.schedule = ImagePiece(os.path.join(template_path, 'schedule.png'))
+        self.event_sep = ImagePiece(os.path.join(template_path, 'item_sep.png'))
+        self.logo = ImagePiece(os.path.join(template_path, 'logo.png'))
+        self.content = [self.header, self.schedule]
+
+    # events should belong to same topic
+    def add_topic(self, events):
+        if not len(events):
+            return
+
+        topic = events[0]['topic']
+        topic_img = ImagePiece(os.path.join(self.settings['template_path'],
+                                            'topic.png'))
+        topic_img.draw_text('Topic: ' + topic,
+                            self.settings['text_style']['topic'])
+        self.content.append(topic_img)
+
+        event_imgs =[self.create_event(event) for event in events]
+        event_seps = [self.event_sep] * len(event_imgs)
+        self.content.extend([img for pair in zip(event_imgs, event_seps)
+                                  for img in pair][:-1])
+
+    def create_event(self, event):
+        event_img = ImagePiece(os.path.join(self.settings['template_path'],
+                                            'item.png'))
+        event_img.draw_text('{} {}'.format(event['date'], event['time']),
+                            self.settings['text_style']['date'])
+        event_img.draw_text(event['session_name'],
+                            self.settings['text_style']['session_name'])
+        event_img.draw_text(event['presenter_name'],
+                            self.settings['text_style']['presenter_name'])
+        event_img.draw_text(event['presenter_title'],
+                            self.settings['text_style']['presenter_title'])
+        event_img.draw_text(event['address1'] + '\n' + event['address2'],
+                            self.settings['text_style']['address'])
+        return event_img
 
     def compose(self):
-        self.composer = self.ImageComposer([self.header,
-                                            self.schedule,
-                                            self.topic,
-                                            self.item,
-                                            self.item_sep])
+        self.content.append(self.tail)
+        composer = ImageComposer(self.content)
+        composer.vertical_combine()
+        return composer.save('./new_post.png')
 
 
 class WhitepaperJournal(App):
     def __init__(self):
         super().__init__()
 
-    def load_input(self):
+    def load_events(self):
         service = GoogleSheet(self.config['GOOGLE'])
         return service.read_as_map(
             self.config['GOOGLE']['SHEET_ID'], 2, 10)
 
-    def create_poster_by_topics(self, events, topics):
-        filtered = [event for event in events if event['topic'] in topics]
-
+    def create_poster(self, events, topics):
+        poster = WhitepaperJournalPoster(self.config['POSTER'])
+        for topic in topics:
+            filtered = [event for event in events if event['topic'] == topic]
+            poster.add_topic(filtered)
+        poster.compose()
 
     def run(self):
-        events = sorted(self.load_input(), key=lambda k: k['date'])
-        self.create_poster_by_topics(
-            events, ['Generalized Mining', 'Consensus'])
+        events = sorted(self.load_events(), key=lambda k: k['date'])
+        self.create_poster(events, ['Generalized Mining', 'Consensus'])
 
 
 def main():
--- toolset/toolset/image/composer.py
+++ toolset/toolset/image/composer.py
@@ -1,15 +1,21 @@
 # -*- encoding: UTF-8 -*-
 
+import os
+import sys
+
 from PIL import ImageFont, ImageDraw, Image
 import numpy as np
+
+FONT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fonts')
 
 
 class ImageComposer:
     def __init__(self, pieces):
-        self.pieces = pieces
+        self.imgs = [p.get_img() for p in pieces]
 
     def vertical_combine(self):
-        self.comb = np.vstack((np.asarray(p) for p in pieces))
+        stacked = np.vstack((np.asarray(img) for img in self.imgs))
+        self.comb = Image.fromarray(stacked)
 
     def save(self, filename):
         self.comb.save(filename)
@@ -18,24 +24,48 @@ class ImageComposer:
 class ImagePiece:
     def __init__(self, filename):
         self.img = Image.open(filename)
-        self.draw = ImageDraw.Draw(img)
 
-    def get_font(font_setting):
-        font_type = font_setting['type']
-        font_size = font_setting['size']
-        return ImageFont.truetype('./fonts/%s.ttf' % font_type, font_size)
+    def get_font(self, font_setting):
+        font_path = os.path.join(FONT_PATH, '%s.ttf' % font_setting['type'])
+        return ImageFont.truetype(font_path, font_setting['size'])
 
-    def draw_one_line(self, text, setting):
+    def word_size(self, font, word):
+        width = 0
+        for c in word:
+            w, h = font.getsize(c)
+            width += w
+        return width, h
+
+    def draw_text(self, text, settings):
         x_pos = settings['x_pos']
         y_pos = settings['y_pos']
-        font = get_font(settings['font'])
-        fill = settings['fill']
-        for c in text:
-            self.draw.text((x_pos, y_pos), c, font=font, fill=fill)
-            w, h = font.getsize(c)
-            x_pos += (w + gap)
+        x_max = settings['x_max']
+        font = self.get_font(settings['font'])
+        gap = settings['gap']
+        fill = tuple(settings['fill'])
 
-    def img(self):
+        draw = ImageDraw.Draw(self.img)
+        for word in text.split(' '):
+            ww, wh = self.word_size(font, word)
+            if x_pos + ww >= x_max:
+                x_pos = settings['x_pos']
+                y_pos += wh
+
+            for c in (word + ' '):
+                if c == '\n' or c == '\r':
+                    x_pos = settings['x_pos']
+                    y_pos += wh
+                    continue
+
+                w, h = font.getsize(c)
+                draw.text((x_pos, y_pos), c, font=font, fill=fill)
+                x_pos += (w + gap)
+
+
+    def draw_img(self, img, setting):
+        pass
+
+    def get_img(self):
         return self.img
 
     def save(self, filename):
