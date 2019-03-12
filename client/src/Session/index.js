3aece472fb350ddd73c39ebd3dcf8aab8f5e6289
Shu Dong <dongs2011@gmail.com>
2019-03-12 08:35:06 +0000

    fix link

--- client/src/Session/index.js
+++ client/src/Session/index.js
@@ -32,37 +32,32 @@ class Session extends React.Component {
       );
   }
 
+  composeLine = (key, value) => {
+    if (typeof value === 'string' && /^https?:\/\//.test(value)) {
+      return '__' + key + '__: [Link](' + value + ')';
+    } else {
+      return '__' + key + '__: ' + value;
+    }
+  }
+
+  composeLines = (title, object, excluded_properties) => {
+    var lines = []
+    lines.push('## ' + title)
+    for (var property in object) {
+      if (excluded_properties.includes(property)) { continue; }
+      if (object.hasOwnProperty(property)) {
+        lines.push(this.composeLine(property, object[property]));
+      }
+    }
+    return lines
+  }
+
   composeInput = (session, presenter) => {
     var lines = [];
-    lines.push('## Session')
-    for (var property in session) {
-      if (property === 'schedule') { continue; }
-      if (session.hasOwnProperty(property)) {
-        lines.push('__' + property + '__: ' + session[property]);
-      }
-    }
-
-    lines.push('## Schedule');
-    for (property in session.schedule) {
-      if (session.schedule.hasOwnProperty(property)) {
-        lines.push('__' + property + '__: ' + session.schedule[property]);
-      }
-    }
-
-    lines.push('## Presenter')
-    for (property in presenter) {
-      if (presenter === 'project') { continue; }
-      if (presenter.hasOwnProperty(property)) {
-        lines.push('__' + property + '__: ' + presenter[property]);
-      }
-    }
-
-    lines.push('## Project')
-    for (property in presenter.project) {
-      if (presenter.project.hasOwnProperty(property)) {
-        lines.push('__' + property + '__: ' + presenter.project[property]);
-      }
-    }
+    lines = lines.concat(this.composeLines('Session', session, ['schedule']));
+    lines = lines.concat(this.composeLines('Schedule', session.schedule, []));
+    lines = lines.concat(this.composeLines('Presenter', presenter, ['project']));
+    lines = lines.concat(this.composeLines('Project', presenter.project, []));
     return lines.join('\n\n')
   }
 
