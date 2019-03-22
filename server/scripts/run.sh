ca6cdfa9ee1044bb5f2c5563ab2a054204c6e76d
Shu Dong <dongs2011@gmail.com>
2019-03-22 21:58:57 +0000

    fix endpoints

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,3 +1,3 @@
 #FLASK_APP=$PWD/endpoints.py FLASK_ENV=development python3 -m flask run --port 4433 --host 0.0.0.0
 
-FLASK_ENV=development python3 endpoints_script.py
+FLASK_ENV=development python3 endpoints.py
